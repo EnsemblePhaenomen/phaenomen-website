4673d1e2546f4c4e21483bb9fb8ef83efd9fb66f
Loulou <sanson.louis@gmail.com>
2025-10-21 22:24:55 +0200

    more styling

--- app/Components/HeroSection/HeroBackground.tsx
+++ app/Components/HeroSection/HeroBackground.tsx
@@ -26,14 +26,14 @@ export default function HeroBackground({
       />
 
       {/* Content overlay */}
-      <div className="absolute inset-0 grid grid-cols-12 grid-rows-12 h-full pointer-events-none">
+      <div className="absolute inset-0 grid grid-cols-12 grid-rows-12 h-full">
         {/* Texte "ensemble"  */}
-        <div className=" col-start-7 col-span-3 row-span-2 row-start-5 flex justify-end items-end bg-green-200 ">
-          <h2 className="hero-title  mb-10">ensemble</h2>
+        <div className=" col-start-7 col-span-3 row-span-2 row-start-4 flex justify-end items-end bg-green-200/20 ">
+          <h2 className="hero-title pb-5 mb-10">ensemble</h2>
         </div>
 
         {/* Logo — occupe 2 cellules en largeur, juste en dessous */}
-        <div className="col-start-5 col-span-5 row-span-4 row-start-6 flex justify-end items-start">
+        <div className="col-start-5 col-span-5 row-span-4 row-start-5 flex justify-end items-start">
           <Image
             src={logo}
             alt="Logo"
@@ -44,19 +44,24 @@ export default function HeroBackground({
           />
         </div>
 
-        <div className="col-start-8 col-span-2 row-start-9 flex justify-end">
+        <div className="col-start-7 col-span-3 row-span-2 row-start-8 flex justify-start bg-blue-200/30 ">
           <div className="flex flex-col items-end leading-none">
-            <p className="text-white text-4xl leading-none">
-              <span>Noé</span>&thinsp;<span>CHAPOLARD</span>
+            <p className="hero-name leading-none">
+              <span>Noé</span>&thinsp;<span>Chapolard</span>
             </p>
-            <p className="text-white text-4xl leading-none mt-2 self-start">
+            <p className=" hero-role mb-4 leading-none mt-2 self-start">
               Direction
             </p>
           </div>
         </div>
-        <div className="cursor-pointer col-start-8 col-span-2 row-start-11 flex justify-start bg-red-200/30">
-          <ArrowIcon className="text-white w-12 h-12 p-2 mt-2" />
-          <p className="text-white text-4xl">Plus d'informations</p>
+        <div className="hover:cursor-pointer  col-start-7 col-span-3 row-start-9 row-span-1 flex justify-start items-center bg-red-200/30">
+          <ArrowIcon className="text-white w-12 h-20 pr-5 mt-2" />
+          <p className="hero-info bg-red-200/30">
+            Plus d'informations
+          </p>
+        </div>
+        <div className="col-start-1 border-white col-span-12 row-start-12 border-t-1">
+          hello
         </div>
 
         {/* Optional children */}
