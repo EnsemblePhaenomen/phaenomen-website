0da024ff96aacf723daffec77c76a07217e6f338
Loulou <sanson.louis@gmail.com>
2025-10-27 12:27:01 +0100

    added logo to header + style to header

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
@@ -11,14 +12,24 @@ export default function Header() {
         pointer-events-auto
 
       "
-      // bg-transparent
+    // bg-transparent
     >
-      <nav className="max-w-screen-xl mx-auto flex gap-6">
-        <Link href="#home" className="hover:underline">Phaenomen</Link>
-        <Link href="/about" className="hover:underline">À propos</Link>
-        <Link href="/contact" className="hover:underline">Contact</Link>
-        <Link href="/agenda" className="hover:underline">Agenda</Link>
-        <Link href="/media" className="hover:underline">Médias</Link>
+      <nav className="w-full mx-auto flex justify-between items-center bg-red-200">
+        <Link href="#home" className="hover:opacity-80 transition-opacity">
+          <Image
+            src="/LOGO_ROSE_SVG.svg"
+            alt="Phaenomen Logo"
+            width={60}
+            height={20}
+            className="h-auto"
+          />
+        </Link>
+        <div className="flex w-1/3 gap-6 bg-blue-200 justify-between">
+          <Link href="/about" className="hover:underline">À propos</Link>
+          <Link href="/contact" className="hover:underline">Contact</Link>
+          <Link href="/agenda" className="hover:underline">Agenda</Link>
+          <Link href="/media" className="hover:underline">Médias</Link>
+        </div>
       </nav>
     </header>
   );
