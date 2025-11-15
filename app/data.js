dee3acd5c309e0d223d0fadc1f98a8111823ec88
Louissnsn <sanson.louis@gmail.com>
2025-11-15 16:49:33 +0100

    updated data.js

--- app/data.js
+++ app/data.js
@@ -1,7 +1,91 @@
 const musiciens = [
-    {
-        nom: "azeaze",
-        prénom: "qdqs",
-        instrument: "qdqsd"
-    }
-]
+  {
+    nom: "Rignol",
+    prénom: "Gabriel",
+    instrument: "Théorbe",
+    bio: [
+      "Né en 2001, Gabriel Rignol débute la guitare à 8 ans au CRR de Perpignan, remportant plusieurs prix. À 16 ans, il se consacre au luth au Conservatoire National Supérieur de Musique et de Danse de Lyon, sous la direction de Rolf Lislevand, obtenant son master en 2023. Gabriel Rignol est membre de différents ensembles baroque prestigieux, enregistrant pour des labels prestigieux (Alpha Classics, Ricercar, Harmonia Mundi, Deutsche Grammophon, etc.). Lauréat du Concours International de Musique Ancienne Maurizio Pratola en 2021, il fonde également l'ensemble la Nébuleuse, axé sur le répertoire français et italien du XVIIe siècle.",
+    ],
+  },
+  {
+    nom: "Mourault",
+    prénom: "Christophe",
+    instrument: "Violon",
+    bio: [
+      `Violoniste, Christophe Mourault se forme auprès de François-Marie Drieux, Marie Rouquié (pôle Aliénor, Poitiers), Enrico Gatti (Conservatorio di Bologna), et Odile Édouard (CNSMD de Lyon). Particulièrement intéressé par les premiers répertoires violonistiques, il rédige un mémoire de recherche sur la diminution et l’ornementation chez les violonistes, au début du 17ème siècle, en Italie.
+        Il se produit régulièrement en concert au sein de diverses formations telles que le quatuor à cordes I Folletti, le trio contr’Arco, et les ensembles Correspondances, Il Caravaggio, Les Surprises, Acanthus baroque, Saint Honoré et Aurea Antiqua.
+`,
+    ],
+  },
+  {
+    nom: "Erzberger",
+    prénom: "David",
+    instrument: "Continuo",
+    bio: [`David Erzberger est claveciniste, organiste et continuiste. Il a d'abord étudié la littérature anglaise à Aberdeen, en Écosse, avant de se consacrer entièrement à la musique. Ses études de clavecin, de théorie musicale, d'improvisation historique et d'orgue/musique sacrée l'ont conduit à Leipzig, Bâle, Lucerne et Fribourg, où il a eu pour professeurs Nicholas Parle et Jean-Christophe Dijoux.
+        David se produit régulièrement avec divers ensembles et orchestres et occupe les fonctions d'organiste et de chef de chœur à Sachseln (Suisse). Il a enseigné la théorie musicale à l’Université de musique de Fribourg et à Karlsruhe, et travaille actuellement comme accompagnateur au clavecin à l'Université de musique de Lucerne tout en enseignant le clavecin à l'École de musique de Zoug.
+`],
+  },
+  {
+    nom: "Palacios",
+    prénom: "Alberto",
+    instrument: "Ténor",
+    bio: [`Dès son plus jeune âge, il a commencé ses études musicales par le piano, obtenant en 2017 le Diplôme Supérieur de Musique et, en 2020, le Master de Lied "Victoria de los Ángeles". Toutefois, son parcours a toujours été marqué par une intense et variée activité chorale, qui a joué un rôle déterminant dans son développement en tant que chanteur.
+
+Il a rapidement décidé d’orienter sa carrière vers le chant, terminant en 2022 le postgrade en Chant Historique à l’ESMuC (Barcelone), où il a obtenu son diplôme avec mention, sous la direction de Lambert Climent et Assumpta Mateu. Cette même année, son engagement a été récompensé par la bourse Pilar Lorengar - Ciudad de Zaragoza.
+
+Son expérience chorale est vaste et diversifiée. Ces dernières années, il a fait partie de prestigieux ensembles tels que les Basler Madrigalisten, le Bach Collegium Barcelona, le Saulus Ensemble, la Capella Nacional de Catalunya dirigée par Jordi Savall et La Cetra dirigée par Andrea Marcon. Il participe également à des ensembles de format réduit comme le Lorem Ipsum Ensemble, le Tetraphilla Ensemble, la Schola Cantorum Paradisi Portae et le cercle actuel des étudiants du master AVES.
+
+Il vient récemment de terminer un Master of Arts à la FHNW (Bâle), où il a étudié sous la direction de Carlos Mena, avec qui il poursuit actuellement sa formation.
+`],
+  },
+  {
+    nom: "Cartier",
+    prénom: "Thierry",
+    instrument: "Baryton",
+    bio: [`C'est à Bordeaux que Thierry Cartier débute ses études de chant lyrique, pour les poursuivre à partir de septembre 2017 au Centre de Musique Baroque de Versailles, où il va se former spécifiquement à l'approche des répertoires baroques. Il intègre ensuite la classe de Céline Laly au conservatoire de Versailles afin de se perfectionner. Il bénéficie des conseils de Chantal Mathias, Sabine Devieilhe et Maarten Konigsberger lors de masterclasses, et se produit régulièrement sous la direction de différents chefs comme Sébastien Daucé (Ensemble Correspondances), Louis-Noël Bestion de Camboulas (Les Surprises), Gaétan Jarry (Marguerite Louise), Marie Van Rhijn (l'Assemblée), enregistre l'intégrale des grands motets de Lully aux côtés de Stéphane Fuget (Les Epopées) et participe à la production des Dialogues des Carmélites de Poulenc à l'opéra de Bordeaux en 2023. 
+
+En 2025, Thierry suit le parcours de la Jeune scène lyrique organisé par l'Arcal, et intègre la promotion 2025-2027 de l'Académie de l'Opéra Royal de Versailles.
+`],
+  },
+  {
+    nom: "Morger",
+    prénom: "Lara",
+    instrument: "Mezzo-soprano",
+    bio: [`La mezzo-soprano suisse Lara Morger se distingue par une grande expressivité, une énergie vibrante et un timbre vocal chaleureux et touchant. En tant que soliste, elle collabore avec des chefs tels que Jordi Savall, Ton Koopman et Shunske Sato, et se produit avec des ensembles comme Hespèrion XXI, Le Concert des Nations, la Lautten Compagney Berlin, l'Orquestra Barroca Catalana et l'Orchestre National du Capitole de Toulouse. Depuis 2021, elle est également membre de la Capella Reial de Catalunya, où elle se produit en soliste sous la direction de Jordi Savall. En 2024, Lara a remporté le 1er prix et le prix du public au 24e Concours international Bach de Leipzig. Elle a également reçu de nombreux prix et bourses, notamment de la Fondation Melante, de la Fondation Richard Wagner et de la Bourse Salvat Bach de la Fondation Bachcelona. Elle a fait ses débuts à l'opéra en 2019 dans le rôle de Diane dans Les Aventures du Roi Pausole d'Arthur Honegger à Fribourg-en-Brisgau. En 2022, elle a chanté le rôle-titre d'Alessandro de Haendel à l'ETA-Hoffmann-Theater de Bamberg sous la direction de Gottfried von der Goltz. Elle a terminé ses études avec distinction à la Hochschule für Musik de Fribourg-en-Brisgau et à la Hochschule der Künste de Berne en 2024. Elle a notamment eu pour professeurs Tanja Ariane Baumgartner, Dorothea Wirtz, Marek Rzepka et Jeanette Favaro-Reuter.`],
+  },
+  {
+    nom: "Lisa",
+    prénom: "Cardonnet",
+    instrument: "Alto",
+    bio: [`Originaire de Montpellier, Lisa Cardonnet commence l’apprentissage de l’alto avec Jacques Aupetit dès son plus jeune âge. Elle poursuit ses études musicales au Conservatoire à Rayonnement Régional de Perpignan dans la classe d’Antoine Dautry, au Conservatoire National Supérieur de Lyon dans la classe de Françoise Gnéri et à la Musikhochschule de Leipzig dans la classe de Tatjana Masurenko. Enfin, c’est à la Haute Ecole de Musique de Lausanne, site de Sion Valais-Wallis qu’ elle obtient son diplôme de Master of Arts en 2021 dans la classe de Tatjana Masurenko. Son activité artistique aux multiples facettes va de la musique baroque à la musique contemporaine en passant par l’improvisation, la création, la médiation et l’enseignement. Lisa se produit autant en soliste qu’au sein de formations de musique de chambre. Ainsi, depuis 2019, elle joue dans l’ensemble Edera dont elle est cofondatrice.`],
+  },
+  {
+    nom: "Bouilloux",
+    prénom: "Véronique",
+    instrument: "Violon",
+    bio: [`Après des études de violon baroque à l'ENM de Villeurbanne auprès de Simon Heyerick, Véronique Bouilloux intègre le CNSMD de Lyon dans la classe d'Odile Edouard où elle obtient son master en 2017. Elle a notamment eu l’occasion de travailler avec Hervé Niquet et Le Concert Spirituel (Fireworks, 2015), Christophe Coin (OFJ 2013), Amandine Beyer, Olivier Schneebeli, Barthold Kuijken et Alessandro Moccia. Elle donne par ailleurs des cours de violon en région lyonnaise et est l’auteur d’un mémoire de recherches sur « Les apports d'un retour aux postures historiques dans la pratique du violon » (CNSMDL 2017).`],
+  },
+  {
+    nom: "Papasergio",
+    prénom: "Manon",
+    instrument: "Viole",
+    bio: [`Musicienne poly-instrumentiste, Manon Papasergio choisit le vaste univers de la musique ancienne pour laisser s’épanouir les différentes facettes de sa personnalité musicale.
+Attirée d’abord par le violoncelle, qu’elle étudie à Caen, elle débute ensuite la harpe et la viole de gambe au conservatoire de Tours. Charmée par la pluralité des voies artistiques qui s’ouvrent à elle, elle choisit de s’y consacrer au Conservatoire National Supérieur de musique et de danse de Lyon. Elle y étudie entre 2019 et 2025 le violoncelle baroque, la harpe ancienne et la viole de gambe auprès d’Emmanuel Balssa, Angélique Mauillon et Myriam Rignol.
+Cet enseignement est enrichi de rencontres musicales et humaines qui la poussent à co-fonder des ensembles tels que le consort de violons Renaissance La Capriola, avec lequel elle obtient un master de musique de chambre en 2024, ou le trio médiéval Ecco la Primavera, spécialisé dans l’élaboration de contes musicaux.
+Appréciée pour ses qualités de continuiste, Manon est appelée en tant que violiste, harpiste ou violoncelliste par de nombreux ensembles de musique ancienne parmi lesquels les Musiciens de Saint-Julien, la Guilde des Mercenaires, la Nébuleuse ou encore Cappella Mediterranea. Remarquée en 2023 lors du concours international de viole de gambe Bach-Abel, dont elle obtient le premier prix, Manon se tourne naturellement vers cet instrument pour son premier enregistrement en tant que soliste, qui lui donne l’occasion d’explorer le répertoire flamboyant et intrigant de la viola bastarda italienne.`],
+  },
+  {
+    nom: "Robinne",
+    prénom: "Maëlys",
+    instrument: "Soprano",
+    bio: [`Maëlys Robinne a commencé sa formation artistique en France, où elle s’est initiée à la fois à la danse et au chant. Sa passion pour la musique ancienne l’a conduite à rejoindre le prestigieux Centre de Musique Baroque de Versailles. Elle a poursuivi ses études de chant lyrique au Pôle Supérieur de Paris et à l’École Supérieure de Musique de Catalogne, à Barcelone, en interprétation classique et en opéra. Récemment, elle a achevé sa formation vocale et musicale avec Carlos Mena à la Schola Cantorum de Bâle.
+Maëlys travaille avec des ensembles internationaux tels que la Capella Nacional (Jordi Savall), le Bach Collegium Japan (Masaaki Suzuki), le Bachcelona Consort (Daniel Tarrida), La Cetra (Andrea Marcon), le Bach Collegium Barcelona (Pau Jorquera) ou Vox Luminis (Lionel Meunier), parmi d’autres. En soliste, elle interprète des œuvres de Vivaldi, Haendel, Pergolesi, Haydn, Mozart, Schubert, Bach, Fauré, Mendelssohn, etc. Son engagement pour l’excellence musicale l’a conduite à recevoir la Bourse Salvat Bach 2023, une distinction qui récompense sa maîtrise du répertoire vocal de Bach.
+`],
+  },
+  {
+    nom: "Chapolard",
+    prénom: "Noé",
+    instrument: "Chef d'orchestre",
+    bio: [],
+  },
+];
