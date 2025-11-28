2f03fd29e98783dea37d8687a5a1d545dfd850ab
Louissnsn <sanson.louis@gmail.com>
2025-11-28 12:41:13 +0100

    working on media

--- app/Components/Sections/MediaSection.tsx
+++ app/Components/Sections/MediaSection.tsx
@@ -1,3 +1,4 @@
+import AnimatedBorderCard from "../(ui)/AnimatedBorderCard";
 import PhotoGallery from "../(ui)/PhotoGallery";
 export default function MediaSection() {
   return (
@@ -11,19 +12,47 @@ export default function MediaSection() {
           <p className="text-xl md:text-2xl leading-relaxed opacity-90 mb-12">
             Explorez notre univers sonore et visuel à travers nos créations.
           </p>
-          <div className="grid md:grid-cols-3 gap-6">
-            <div className="bg-white/10 backdrop-blur-sm rounded-lg p-6">
-              <h3 className="text-xl font-semibold mb-4">Audio</h3>
-              <p className="opacity-80">Enregistrements et créations sonores</p>
-            </div>
-            <div className="bg-white/10 backdrop-blur-sm rounded-lg p-6">
-              <h3 className="text-xl font-semibold mb-4">Vidéo</h3>
-              <p className="opacity-80">Captations</p>
-            </div>
-            <div className="bg-white/10 backdrop-blur-sm rounded-lg p-6">
-              <h3 className="text-xl font-semibold mb-4">Photos</h3>
-              <p className="opacity-80">Galerie</p>
-            </div>
+          <div className="flex flex-col gap-6">
+            
+            {/* AUDIO SI UN JOUR IL Y EN A */}
+            {/* <AnimatedBorderCard
+            className="w-full"
+            sides={["top",]}
+            animationDuration={0.3}
+            delay={0.2}
+          >
+              <div className="rounded-lg p-6">
+                <h3 className="text-xl font-semibold mb-4">Audio</h3>
+                <p className="opacity-80">Enregistrements et créations sonores</p>
+              </div>
+            </AnimatedBorderCard> */}
+
+            {/* VIDEOS SECTION */}
+
+            <AnimatedBorderCard
+              className="w-full"
+              sides={["top"]}
+              animationDuration={0.3}
+              delay={0.2}
+            >
+              <div className="rounded-lg p-6">
+                <h3 className="text-xl font-semibold mb-4">Vidéo</h3>
+                <p className="opacity-80">Captations</p>
+              </div>
+            </AnimatedBorderCard>
+            {/* PHOTOS SECTION */}
+
+            <AnimatedBorderCard
+              className="w-full"
+              sides={["top"]}
+              animationDuration={0.3}
+              delay={0.2}
+            >
+              <div className="rounded-lg p-6">
+                <h3 className="text-xl font-semibold mb-4">Photos</h3>
+                <p className="opacity-80">Galerie</p>
+              </div>
+            </AnimatedBorderCard>
           </div>
           <div>
             <PhotoGallery />
