76a9d808f2bfae47076f0353d4b6619e4476a787
Louissnsn <sanson.louis@gmail.com>
2025-12-02 13:11:16 +0100

    added contact info

--- app/Components/Sections/ContactSection/LegalInfoContact.tsx
+++ app/Components/Sections/ContactSection/LegalInfoContact.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function LegalInfoContact() {
   return (
     <div className="w-full mt-16 md:mt-20 px-8">
@@ -26,6 +28,18 @@ export default function LegalInfoContact() {
           <p>
             <span className="font-semibold font-sm">Hébergeur :</span> OVH SAS –
             2 rue Kellermann, 59100 Roubaix, France
+          </p>
+
+          <p className="mt-2">
+            <span className="font-semibold font-sm">Développement du site : </span>
+            <Link
+              href="https://www.louissanson.dev"
+              target="_blank"
+              rel="noreferrer"
+              className="underline underline-offset-4 hover:text-black transition-colors"
+            >
+              Louis Sanson
+            </Link>
           </p>
         </div>
       </div>
