96f7d98968033524c6296ebfc446b5a2c7a930f4
Louissnsn <sanson.louis@gmail.com>
2025-12-02 13:09:00 +0100

    added contact info

--- app/Components/Sections/ContactSection/LegalInfoContact.tsx
+++ app/Components/Sections/ContactSection/LegalInfoContact.tsx
@@ -1,9 +1,11 @@
 export default function LegalInfoContact() {
   return (
-    <div className="w-full mt-16 md:mt-24 px-8">
-      <div className="max-w-4xl mx-auto text-center space-y-2 text-neutral-600 text-sm md:text-base leading-relaxed">
+    <div className="w-full mt-16 md:mt-20 px-8">
+      <div className="max-w-4xl mx-auto text-center space-y-2 text-neutral-600  leading-relaxed">
+        <div className="text-sm md:text-xl">
+
         <p>
-          <span className="font-semibold">Coordination de la recherche :</span>{" "}
+          <span className="font-semibold">Coordination de la recherche : </span>
           Claire Laplace
         </p>
 
@@ -11,11 +13,21 @@ export default function LegalInfoContact() {
           <span className="font-semibold">Siège social :</span> 16 rue du Mont
           d’Or, 69009 Lyon, France
         </p>
+        </div>
 
-        <p>
-          <span className="font-semibold">Hébergeur :</span> OVH SAS – 2 rue
-          Kellermann, 59100 Roubaix, France
-        </p>
+        <div className="mt-5 text-sm md:text-base">
+          <p>
+            <span className="font-semibold font-sm">
+              Directrice de la publication :{" "}
+            </span>
+            Claire Laplace
+          </p>
+
+          <p>
+            <span className="font-semibold font-sm">Hébergeur :</span> OVH SAS –
+            2 rue Kellermann, 59100 Roubaix, France
+          </p>
+        </div>
       </div>
     </div>
   );
